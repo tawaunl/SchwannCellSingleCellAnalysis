f0a216a0f57d5f43542ce19c848dd5d115c8f0c3
tawaunl <86265976+tawaunl@users.noreply.github.com>
2021-06-21 16:21:21 -0700

    Add files via upload

--- SchwannCellSingleCell.R
+++ SchwannCellSingleCell.R
@@ -5,7 +5,6 @@ library(patchwork)
 
 setwd("~/Downloads/SchwannCellSingleCellAnalysis/")
 
-
 # Loading P1 data
 cts <- list()
 cts[[1]] <- ReadMtx(mtx ="GSE138577_RAW/P1_1/GSM4113877_10X_P1_1_matrix.mtx.gz",
@@ -36,4 +35,29 @@ P1data[["percent.mt"]] <- PercentageFeatureSet(P1data, pattern = "^MT-")
 # Visualize QC metrics as a violin plot
 p <- VlnPlot(P1data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
-savePlot(filename = file.path(res))
+savePlot(p,filename = "ViolinPlot_P1.pdf")
+
+
+P1data <- subset(P1data,
+                 subset = nFeature_RNA > 500 & nFeature_RNA < 5000 & percent.mt < 25)
+
+P1data <- NormalizeData(object = P1data, normalization.method = "LogNormalize",
+                        scale.factor = 10000)
+
+P1data <- FindVariableFeatures(P1data,
+                               selection.method = "vst", nfeatures = 2000)
+
+
+
+P1data <- ScaleData(P1data, features = rownames(P1data))
+P1data <- RunPCA(P1data, features = VariableFeatures(object =P1data), print = TRUE,
+                 pcs.print = 1:5, genes.print = 5)
+
+#P1data <- ProjectPCA(object = P1data, do.print = FALSE)
+P1data <- FindNeighbors(P1data, reduction = "pca", dims = 1:10,
+                       resolution = 0.5, verbose = 0, prune.SNN = 0/100,
+                       algorithm = 3, force.recalc=TRUE)
+
+set.seed(100)
+P1data <- RunTSNE(object = P1data, dims.use = 1:10, do.fast = TRUE, perplexity = 100)
+saveRDS(P1data, file="P1data.rds")
